45db517f0ebb2a473648681ef36d89f5889d088d
azizasall <elhadji-abdou-aziz.sall.1@ulaval.ca>
2020-11-04 10:11:43 -0500

    2nd commit delete doublon or duplicate

--- traitement_R.R
+++ traitement_R.R
@@ -52,6 +52,26 @@ colnames(data_2018.2.df)
 
 
 
+
+# importer_ratings --------------------------------------------------------
+
+library(readxl)
+X454_ratings <- read_excel("C:/Users/aziza/Desktop/Session_3/Projet de fin d'études en gestion financière/data_traiement_dans_R/FINAL_DATA/FINAL_DATA/FINAL_DATA_ESSAI/454_ratings.xlsx", 
+                           col_names = FALSE)
+View(X454_ratings)
+
+
+#extraction ratings
+ratings <- X454_ratings[5:nrow(X454_ratings) , 3]
+
+ratings
+
+
+#cbind ratings et data_2018.2.df
+
+data_2018.2.df <- cbind(ratings, data_2018.2.df)
+fix(data_2018.2.df)
+
 # suppression_des_rows_with_not_data ---------------------------------
 
 #je sais que j'ai 35 variables donc je construit une new database sans
@@ -61,9 +81,9 @@ colnames(data_2018.2.df)
 #la somme des NA de chaque row dans la database
 rowSums(is.na(data_2018.2.df))
 
-#si on a plus de 35 NA on l'exclus le row (sachant qu'on a 35 variables)
+#si on a plus de 35 NA on delete le row (sachant qu'on a 35 variables)
 data_2018.3.df <- data_2018.2.df[rowSums(is.na(data_2018.2.df))<35,]
-
+fix(data_2018.3.df)
 
 
 
@@ -84,10 +104,63 @@ colSums(is.na(data_2018.3.df))
 data_2018.4.df <- data_2018.3.df[ ,colSums(is.na(data_2018.3.df))<228]
 dim(data_2018.4.df) # il me reste 24 variables
 
+fix(data_2018.4.df)
+
+x <- colnames(data_2018.4.df)
 
 
 
 
+# Delete_duplicate_ou_doublons_en_FR --------------------------------------
+
+data_2018.5.df <- data_2018.4.df[!duplicated(data_2018.4.df[c("PROF_MARGIN", "EBITDA_MARGIN")]),]
+dim(data_2018.5.df)
+fix(data_2018.5.df)
+
+
+
+# impute_NA_value_avec_mice -----------------------------------------------
+
+library(mice)
+
+#voir le type de mes données
+summary(data_2018.5.df[,2:ncol(data_2018.5.df)])  #données sont des character donc changer en numérique
+
+#changement type character en numeric
+
+library(dplyr)
+
+x <- colnames(data_2018.5.df)
+
+as.vector(x)
+
+#quand je fais select(PROF_MARGIN)%>%  ça marche
+
+
+  data_2018.5.df %>%
+  select(GEO_GROW_TOT_ASSET)%>%
+  unlist()%>%
+  class()
+
+
+
+  
+  data_2018.5.df %>%
+    select(starts_with("PROF_MARGIN"))%>%
+    select(ends_with("GEO_GROW_TOT_ASSET"))%>%
+    unlist()%>%
+    class()
+  
+
+mean(data_2018.5.df$EBITDA_TO_REVENUE) # see message d'erreur
+
+summary(data_2018.5.df)
+
+
+
+summary(data_2018.5.df[-1])
+
+str(data_2018.5.df)
 
 
 
@@ -111,9 +184,18 @@ apply(data_2018.4.df, 2, p)
 ######################################################################
 
 
+#####################################done!############################
+# mercredi : (1)  delete doublons 
+######################################################################
 
 
-##################
-# mercredi : (1) impute NA value avec mice, (2) convertir ratings, 
+
+
+#*************OBJECTIF_AVANT_VENDREDI_BIEN-AVANCER*************************
+
+
+
+            #(1.1)impute NA value avec mice, 
+             # (2) convertir ratings, 
              # (3) constituer good ratios, (4) faire 1st régression
 ##################
